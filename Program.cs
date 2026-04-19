b86d6458617ca26b3469cc72a476b0194bc67252
Amir Ali <240560130+AmirAli1019@users.noreply.github.com>
2026-04-19 11:59:07 +0330

    Added multitasking

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ class Program
         HttpClient client = new();
         client.Timeout = new TimeSpan(0, 0, 15);
 
-        string[] sites = f.ReadToEnd().Split("\n");
+        string[] sites = f.ReadToEnd().Split("\n", StringSplitOptions.RemoveEmptyEntries);
 
         Table table = new();
         table.AddColumns("Website", "ICMP", "HTTP");
@@ -19,39 +19,63 @@ class Program
         const string OK = "[green]OK[/]";
         const string FAILED = "[red]FAILED[/]";
 
+        const int MAX_CONCURRENT_THREADS = 5;
+
+        var semaphore = new SemaphoreSlim(MAX_CONCURRENT_THREADS);
+        var tasks = new List<Task>();
+
         foreach (string site in sites)
         {
             if (string.IsNullOrWhiteSpace(site))
                 continue;
 
-            Console.WriteLine($"Pinging {site} ...");
-            Process p = Process.Start(
-                new ProcessStartInfo
-                {
-                    FileName = "ping",
-                    Arguments = $"-c 3 -W 1 {site}",
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                }
-            )!;
-            p.WaitForExit();
+            await semaphore.WaitAsync();
 
-            HttpResponseMessage response = new(System.Net.HttpStatusCode.Unauthorized);
-
-            try
+            var task = Task.Run(async () =>
             {
-                Console.WriteLine($"Getting {site} ...");
-                response = await client.GetAsync($"https://{site}");
-            }
-            catch { }
+                try
+                {
+                    Console.WriteLine($"Pinging {site} ...");
+                    Process p = Process.Start(
+                        new ProcessStartInfo
+                        {
+                            FileName = "ping",
+                            Arguments = $"-c 3 -W 1 {site}",
+                            RedirectStandardOutput = true,
+                            RedirectStandardError = true,
+                            UseShellExecute = false,
+                        }
+                    )!;
+                    p.WaitForExit();
 
-            table.AddRow(
-                $"[blue]{site}[/]",
-                p.ExitCode == 0 ? OK : FAILED,
-                response.IsSuccessStatusCode ? OK : FAILED
-            );
+                    HttpResponseMessage response = new(System.Net.HttpStatusCode.Unauthorized);
+
+                    try
+                    {
+                        Console.WriteLine($"Getting {site} ...");
+                        response = await client.GetAsync($"https://{site}");
+                    }
+                    catch { }
+
+                    lock (table)
+                    {
+                        table.AddRow(
+                            $"[blue]{site}[/]",
+                            p.ExitCode == 0 ? OK : FAILED,
+                            response.IsSuccessStatusCode ? OK : FAILED
+                        );
+                    }
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            });
+
+            tasks.Add(task);
         }
+
+        await Task.WhenAll(tasks);
 
         AnsiConsole.Write(table);
     }
