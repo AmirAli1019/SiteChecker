664a77e6f2d7e8cd779b363674a006d6575ce71f
Amir Ali <240560130+AmirAli1019@users.noreply.github.com>
2026-04-20 23:39:31 +0330

    Added partial result displaying ability after receiving interrupt

--- Program.cs
+++ Program.cs
@@ -4,24 +4,81 @@ using Spectre.Console;
 
 class Program
 {
+    static Table table = new();
+
+    const string OK = "[green]OK[/]";
+    const string FAILED = "[red]FAILED[/]";
+
+    static HttpClient client = new();
+
+    const int MAX_CONCURRENT_THREADS = 5;
+    static SemaphoreSlim semaphore = new SemaphoreSlim(MAX_CONCURRENT_THREADS);
+
+    static void ShowResults()
+    {
+        AnsiConsole.Write(table);
+    }
+
+    static async Task TestWebsite(string site)
+    {
+        try
+        {
+            Console.WriteLine($"Pinging {site} ...");
+            Process p = Process.Start(
+                new ProcessStartInfo
+                {
+                    FileName = "ping",
+                    Arguments = $"-c 3 -W 1 {site}",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                }
+            )!;
+            p.WaitForExit();
+
+            HttpResponseMessage response = new(System.Net.HttpStatusCode.Unauthorized);
+
+            try
+            {
+                Console.WriteLine($"Getting {site} ...");
+                response = await client.GetAsync($"https://{site}");
+            }
+            catch { }
+
+            lock (table)
+            {
+                table.AddRow(
+                    $"[blue]{site}[/]",
+                    p.ExitCode == 0 ? OK : FAILED,
+                    response.IsSuccessStatusCode ? OK : FAILED
+                );
+            }
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+
     static async Task Main(string[] args)
     {
+        Console.CancelKeyPress += (object? sender, ConsoleCancelEventArgs args) =>
+        {
+            AnsiConsole.MarkupLine(
+                "[yellow]\nReceived interrupt. Showing partial results ...\n[/]"
+            );
+
+            ShowResults();
+        };
+
         using StreamReader f = new(args[0]);
 
-        HttpClient client = new();
         client.Timeout = new TimeSpan(0, 0, 15);
 
         string[] sites = f.ReadToEnd().Split("\n", StringSplitOptions.RemoveEmptyEntries);
 
-        Table table = new();
         table.AddColumns("Website", "ICMP", "HTTP");
 
-        const string OK = "[green]OK[/]";
-        const string FAILED = "[red]FAILED[/]";
-
-        const int MAX_CONCURRENT_THREADS = 5;
-
-        var semaphore = new SemaphoreSlim(MAX_CONCURRENT_THREADS);
         var tasks = new List<Task>();
 
         foreach (string site in sites)
@@ -31,52 +88,13 @@ class Program
 
             await semaphore.WaitAsync();
 
-            var task = Task.Run(async () =>
-            {
-                try
-                {
-                    Console.WriteLine($"Pinging {site} ...");
-                    Process p = Process.Start(
-                        new ProcessStartInfo
-                        {
-                            FileName = "ping",
-                            Arguments = $"-c 3 -W 1 {site}",
-                            RedirectStandardOutput = true,
-                            RedirectStandardError = true,
-                            UseShellExecute = false,
-                        }
-                    )!;
-                    p.WaitForExit();
-
-                    HttpResponseMessage response = new(System.Net.HttpStatusCode.Unauthorized);
-
-                    try
-                    {
-                        Console.WriteLine($"Getting {site} ...");
-                        response = await client.GetAsync($"https://{site}");
-                    }
-                    catch { }
-
-                    lock (table)
-                    {
-                        table.AddRow(
-                            $"[blue]{site}[/]",
-                            p.ExitCode == 0 ? OK : FAILED,
-                            response.IsSuccessStatusCode ? OK : FAILED
-                        );
-                    }
-                }
-                finally
-                {
-                    semaphore.Release();
-                }
-            });
+            var task = Task.Run(() => TestWebsite(site));
 
             tasks.Add(task);
         }
 
         await Task.WhenAll(tasks);
 
-        AnsiConsole.Write(table);
+        ShowResults();
     }
 }
